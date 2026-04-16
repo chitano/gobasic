cef463a22fed57944e6096c30184d1bc79164d3b
Chitano Kumar Sarker <chitanokumar@gmail.com>
2026-04-17 00:40:48 +0600

    variable example added

--- examples/02-variable/main.go
+++ examples/02-variable/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	var name string = "Chitano Kumar" //Explicitly define type.
+	fmt.Println("My name is", name)
+
+	age := 34 //Auto detected type or type inferance featrue in Golang
+
+	fmt.Println("My age is", age)
+
+	email := "chitanokumar@gmail.com"
+
+	count, _ := fmt.Println(email)
+
+	fmt.Println("Number of character in my email", count)
 
 }
